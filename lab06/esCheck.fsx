3c6ba0c67f6af42496783455afabbd458c265b0f
Marco Comes <marco.comes@studenti.unimi.it>
2018-03-19 13:08:30 +0100

    Es 6 1.1

---
+++ lab06/esCheck.fsx
@@ -0,0 +1,25 @@
+﻿#r "FsCheck";;
+#r "FsCheck";;
+open FsCheck;;
+
+// 1.1
+let rec rmEven ls =
+    match ls with
+    | [] -> []
+    | hd :: tl ->
+        match hd % 2 with
+        | 0 -> rmEven tl
+        | _ -> hd :: rmEven tl
+
+let rec containsOddsOnly ls =
+    match ls with
+    | [] -> true
+    | hd :: tl ->
+        match hd % 2 with
+        | 0 -> false
+        | _ -> containsOddsOnly tl
+
+let prop_oddsOnly xs =
+    rmEven xs |> containsOddsOnly
+
+do Check.Quick prop_oddsOnly
