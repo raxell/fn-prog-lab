3c61b96fa7ec732f7b7d10afc95dd65d8cfa03fe
Marco Comes <marco.comes@studenti.unimi.it>
2018-03-23 11:48:34 +0100

    Lab 7 init

---
+++ lab07/binTree.fsx
@@ -0,0 +1,15 @@
+#r "FsCheck"
+open FsCheck
+
+type 'a binTree =
+    | Null
+    | Node of 'a  * 'a binTree * 'a binTree
+
+let t2 = Node(2, Null, Node(4, Null, Null))
+let t7 = Node(7, Null, Node(10, Null, Node(13, Null, Null)))
+let t8 = Node(8, Node(11, Null, Null), Null)
+let t5 = Node(5, t7, t8 )
+let t9 = Node(9, Null, Node (12, Null, Null))
+let t6 = Node(6, t9, Null)
+let t3 = Node(3, t5, t6)
+let t1 = Node(1, t2, t3)
