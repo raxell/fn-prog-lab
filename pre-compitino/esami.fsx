4a5cf4a896f97ccc688ea77aabe7e25e9f44e724
Marco Comes <m.comes@gmail.com>
2018-03-26 15:58:53 +0200

    pre-compitino parte A es 1

--- pre-compitino/esami.fsx
+++ pre-compitino/esami.fsx
@@ -2,3 +2,12 @@
 type valV = { studente: string; voto: int }
 type valG = { studente: string; giudizio: string }
 
+// Es 1
+let valuta { studente = s; voto = v } =
+    let r = { studente = s; giudizio = "" }
+    match v with
+    | x when x > 27 -> { r with giudizio = "ottimo" }
+    | x when x > 22 -> { r with giudizio = "buono" }
+    | x when x > 17 -> { r with giudizio = "sufficiente" }
+    | _ -> { r with giudizio = "insufficiente" }
+
