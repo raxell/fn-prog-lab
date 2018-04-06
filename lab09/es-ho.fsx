8478547c69d0859f3a68b79417970d7214f16791
Marco Comes <comes95@gmail.com>
2018-04-06 12:33:17 +0200

    Es 9 2 QuickCheck

--- lab09/es-ho.fsx
+++ lab09/es-ho.fsx
@@ -1,4 +1,6 @@
-// Es 9 2.1
+#r "FsCheck"
+open FsCheck// Es 9 2.1
+
 let rec map f ls =
 	match ls with
 	| [] -> []
@@ -12,4 +14,14 @@ let l3 = map (fun x -> (x, if x % 2 = 0 then "pari" else "dispari")) l1
 // Es 9 2.3
 let names = [("Mario", "Rossi"); ("Anna Maria", "Verdi"); ("Giuseppe", "Di Gennaro")]
 let names1 = map (fun (x, y) -> "Dott. " + x + " " + y) names
+
+let prop_map f (ls: int list) =
+    map f ls = List.map f ls
+
+do Check.Quick prop_map
+
+let prop_map_preserve_len f (ls: int list) =
+     List.length (map f ls) =  List.length (List.map f ls)
+
+do Check.Quick prop_map_preserve_len
 
