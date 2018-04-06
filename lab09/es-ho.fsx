ad11e828128ba6edcdd306c99acc541876fc735c
Marco Comes <comes95@gmail.com>
2018-04-06 12:20:07 +0200

    Es 9 2.3

--- lab09/es-ho.fsx
+++ lab09/es-ho.fsx
@@ -9,3 +9,7 @@ let l1 = [1..10]
 let l2 = map (fun x -> x * x) l1
 let l3 = map (fun x -> (x, if x % 2 = 0 then "pari" else "dispari")) l1
 
+// Es 9 2.3
+let names = [("Mario", "Rossi"); ("Anna Maria", "Verdi"); ("Giuseppe", "Di Gennaro")]
+let names1 = map (fun (x, y) -> "Dott. " + x + " " + y) names
+
