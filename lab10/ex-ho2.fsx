769dce1bd9ecd5fcf0c20b86bcc27dfdb5c11c33
Marco Comes <comes95@gmail.com>
2018-04-11 11:51:25 +0200

    Es 10 1.3

--- lab10/ex-ho2.fsx
+++ lab10/ex-ho2.fsx
@@ -17,3 +17,11 @@ let prop_filter p ls =
 
 do Check.Quick prop_filter
 
+// Es 1.3
+let unzip ls = List.foldBack (fun (x, y) (xs, ys) -> (x :: xs, y :: ys)) ls ([], [])
+
+let prop_unzip ls =
+    List.unzip ls = unzip ls
+
+do Check.Quick prop_unzip
+
