7e5bdef6cf2024590e1e6986fd8c5d177ddcc78d
Marco Comes <comes95@gmail.com>
2018-04-16 11:53:22 +0200

    Es 11 1 length

--- lab11/iter-ex.fsx
+++ lab11/iter-ex.fsx
@@ -16,3 +16,17 @@ let prop_sum ls =
 
 do Check.Quick prop_sum
 
+let itLength ls =
+    let rec auxLength acc xs =
+        match xs with
+        | [] -> acc
+        | hd :: tl -> auxLength (1 + acc) tl
+    auxLength 0 ls
+
+let itfLength ls = List.fold (fun x _ -> 1 + x) 0 ls
+
+let prop_length ls =
+    itLength ls = List.length ls && List.length ls = itfLength ls
+
+do Check.Quick prop_length
+
